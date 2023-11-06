27076752e94d402898b8d9c67e62ef9f075341e2
Kaleb-Nim <65989471+Kaleb-Nim@users.noreply.github.com>
2023-11-06 10:37:03 +0800

    Update chains

--- llm/chains.py
+++ llm/chains.py
@@ -8,14 +8,37 @@ prompt = PromptTemplate(
     template = """Role: You are a question checker. Given valid questions, match the user question to one of the questions based on contextual meaning. 
     If none of the given questions match, return False
 
+    Example 1:
+    Questions:
+    ---
+    1.1) Is your loved one with dementia restless? 
+    1.2) Does your loved one with dementia seem sad or upset? 
+    1.3) Does your loved one with dementia seem to be looking for something?
+    ---
+    2.1) What are the psychological impacts of tube feeding? 
+    2.2) tube feeding impacts
+    2.3) psychological impacts of tube feeding
+
+    User Question: 
+    "potential impacts of tube feeding for my grandma"
+
+    Your Answer:
+    {
+        "isValidQuestion": true,
+        "matched_question": "tube feeding impacts",
+        "question_list_index": 2
+    }
+
+    ------------------------------------------------------------------------------------------------------
     Questions: 
     {questions}
 
     User Question: 
     {user_question}
 
-    Is the user question valid? Valid questions are questions the user_question that has the same contextual meaning as one of the questions in the list of questions:
-    DO NOT RE-PHRASE THE QUESTION. Return the question as is. If the user_question doesn't match any of the questions, return 'no matched':
+    Task:
+    1. Check if the user question is valid. Valid questions are questions the user_question that has the same contextual meaning as at least one of the questions in the list of questions
+    2. Given the document questions, match the user question to one of the questions based on contextual meaning.
     """
 )
 llm = ChatOpenAI(model_name='gpt-3.5-turbo',temperature=0)
@@ -25,16 +48,16 @@ output_schema = {
     "description": "Given valid questions, match the user question to one of the questions based on contextual meaning. If none of the given questions match, return False else return the matched question without re-phrasing. ",
     "type": "object",
     "properties": {
-      "isValidQuestion": {
-        "type": "boolean",
-        "description": "Is the user question valid?, True or False. Valid questions are questions the user_question that has the same contextual meaning as one of the questions in the list of questions"
-      },
       "matched_question":{
         "type": "string",
         "description": "Which exact question is the user question? DO NOT RE-PHRASE THE QUESTION. Return the question as is. If the user_question doesn't match any of the questions, return 'no matched'"
       },
+      "question_list_index":{
+        "type": "integer",
+        "description": "Which question list is the user question? If the user_question doesn't match any of the questions, return -1"
+      }
     },
-    "required": ["isValidQuestion","matched_question"]
+    "required": ["matched_question","question_list_index"]
 }
 
 output_chain = create_structured_output_chain(llm=llm,prompt = prompt,output_schema=output_schema)
